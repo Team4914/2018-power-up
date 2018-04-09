1d76260b6e9975fac06bd15ba95220a46fecd9a3
Michael Shi <1027michael@gmail.com>
2018-04-08 23:58:37 -0400

    Final commit

--- Accelerometer/src/org/usfirst/frc/team4914/robot/Robot.java
+++ Accelerometer/src/org/usfirst/frc/team4914/robot/Robot.java
@@ -15,6 +15,7 @@ import edu.wpi.first.wpilibj.SPI.Port;
 import edu.wpi.first.wpilibj.SampleRobot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.interfaces.Accelerometer;
+import edu.wpi.first.wpilibj.interfaces.Accelerometer.Range;
 
 public class Robot extends SampleRobot{
 	ADXL345_I2C accel;
@@ -22,11 +23,11 @@ public class Robot extends SampleRobot{
 	SPI gyro;
 	long thing;
 	
-	double builtOffsetY;
+	double builtAverageY;
 	
-	double offsetX;
-	double offsetY;
-	double offsetZ;
+	double averageX;
+	double averageY;
+	double averageZ;
 	
 	double accelerationX;
 	double accelerationY;
@@ -35,6 +36,9 @@ public class Robot extends SampleRobot{
 	double builtAccelX;
 	double builtAccelY;
 	double builtAccelZ;
+	
+	double builtVelocityY = 0;
+	double builtPosY = 0;
 	
 	double velocityX = 0;
 	double velocityY = 0;
@@ -52,45 +56,54 @@ public class Robot extends SampleRobot{
 		accel = new ADXL345_I2C(I2C.Port.kOnboard, Accelerometer.Range.k4G);
 		gyro = new SPI(Port.kOnboardCS0);
 		builtAccel = new BuiltInAccelerometer();
+		builtAccel.setRange(Range.k2G);
 	}
 	
 	@Override
 	public void operatorControl() {
-		offsetX = accel.getX();
-		offsetY = accel.getY();
-		offsetZ = accel.getZ();
 		
-		builtOffsetY = builtAccel.getY();
+		double offsetY = builtAccel.getY();
+		
+		averageX = accel.getX();
+		averageY = accel.getY();
+		averageZ = accel.getZ();
+		
+		builtAverageY = builtAccel.getY();
 		while(isEnabled()){
 			thing = gyro.getAccumulatorValue();
 			
-			accelerationX = accel.getX() - offsetX;
-			accelerationY = accel.getY() - offsetY;
-			accelerationZ = accel.getZ() - offsetZ;
+			accelerationX = accel.getX() - averageX;
+			accelerationY = accel.getY() - averageY;
+			accelerationZ = accel.getZ() - averageZ;
 			
 			builtAccelX = builtAccel.getX();
-			builtAccelY = builtAccel.getY() - builtOffsetY;
+			builtAccelY = (builtAccel.getY() - builtAverageY) - offsetY;
 			builtAccelZ = builtAccel.getZ();
 			
-			velocityX += accelerationX * 0.01;
-			velocityY += accelerationY * 0.01;
-			velocityZ += accelerationZ * 0.01;
+			velocityX += accelerationX * 0.001;
+			velocityY += accelerationY * 0.001;
+			velocityZ += accelerationZ * 0.001;
 			
-			posX += velocityX * 0.01;
-			posY += velocityY * 0.01;
-			posZ += velocityZ * 0.01;
+			posX += velocityX * 0.001;
+			posY += velocityY * 0.001;
+			posZ += velocityZ * 0.001;
 			
-			velocityX = 0;
-			velocityY = 0;
-			velocityZ = 0;
+			builtVelocityY += builtAccelY * 0.00125;
+			builtPosY += builtVelocityY * 0.00125;
 			
+			//end
+			averageX = accelerationX * 0.1 + 0.9 * averageX;
+			averageY = accelerationY * 0.1 + 0.9 * averageY;			
+			averageZ = accelerationZ * 0.1 + 0.9 * averageZ;
+			
+			builtAverageY = builtAccelY * 0.01 + 0.99 * builtAverageY;
 			//System.out.println("thing:" + thing);
 			
 			//System.out.println("X: " + accelerationX);
-			System.out.println("Y: " + builtAccelY);
-			System.out.println("X: " + accelerationX * -1);
+			//System.out.println("Y: " + builtAccelY);
+			System.out.println("Y: " + builtPosY);
 			//System.out.println("Z: " + posZ);
-			Timer.delay(0.01);
+			Timer.delay(0.00125);
 			
 			//System.out.println(posX);
 		}
